21496e6202630844e00fc2806463f68dc9a3558f
IvanShalimov <gogi-mogel@mail.ru>
2018-03-02 10:45:38 +0300

    init project

---
+++ app/src/main/java/com/example/ivan/personaltimer/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ivan.personaltimer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
---
+++ app/src/main/java/com/example/ivan/personaltimer/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.ivan.personaltimer
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        Log.d("Test","onClick")
+        //1000*60*60 - hour
+        val intent = Intent(applicationContext, AlarmActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(applicationContext,0,intent,0)
+
+        val am:AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        am.set(AlarmManager.RTC, AlarmManager.INTERVAL_HOUR, pendingIntent)
+
+    }
+}
