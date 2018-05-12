8e0cfb533b1d1ae0f9366e82c5ee94cbf233df3a
IvanShalimov <gogi-mogel@mail.ru>
2018-05-12 21:28:56 +0300

    music code transport to service.

--- app/src/main/java/com/example/ivan/personaltimer/AlarmActivity.kt
+++ app/src/main/java/com/example/ivan/personaltimer/AlarmActivity.kt
@@ -2,27 +2,25 @@ package com.example.ivan.personaltimer
 
 import android.app.AlarmManager
 import android.content.Context
-import android.media.MediaPlayer
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 
+
 class AlarmActivity : AppCompatActivity(){
 
-    private val mediaPlayer:MediaPlayer by lazy {
-        MediaPlayer.create(this,R.raw.topsy_turvy_s_like_a_living_dead)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-        mediaPlayer.isLooping = true
-        mediaPlayer.start()
+        startService(Intent(this,MusicService::class.java))
     }
 
     fun onClick(v: View?) {
-        releaseMediaPlayer()
+
+        stopService(Intent(this,MusicService::class.java))
 
         if(v?.id == R.id.textView)
                 (getSystemService(Context.ALARM_SERVICE) as AlarmManager)
@@ -31,17 +29,5 @@ class AlarmActivity : AppCompatActivity(){
                                 CreaterPendingIntent.create(applicationContext))
 
         finish()
-
     }
-
-    private fun releaseMediaPlayer(){
-        mediaPlayer.stop()
-        mediaPlayer.release()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        releaseMediaPlayer()
-    }
-
 }
---
+++ app/src/main/java/com/example/ivan/personaltimer/MusicService.kt
@@ -0,0 +1,44 @@
+package com.example.ivan.personaltimer
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MusicService : Service(), MediaPlayer.OnCompletionListener {
+    override fun onCompletion(mp: MediaPlayer?) {
+        //releaseMediaPlayer()
+        stopSelf()
+    }
+
+    private val mediaPlayer: MediaPlayer by lazy {
+        MediaPlayer.create(this,R.raw.topsy_turvy_s_like_a_living_dead)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer.isLooping = false
+        mediaPlayer.setOnCompletionListener(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if(!mediaPlayer.isPlaying)
+            mediaPlayer.start()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releaseMediaPlayer()
+    }
+
+    private fun releaseMediaPlayer(){
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+}
